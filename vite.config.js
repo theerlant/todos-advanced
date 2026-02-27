7df6b81f9154ca769f4584461ddebcb171b49455
theerlant <ttrinanda2@gmail.com>
2026-02-28 01:31:06 +0800

    fix: added vitePWA workbox pattern to cache so it will cache required files properly

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
       manifest: {
         name: "Todos Advanced",
         short_name: "Todos Advanced",
